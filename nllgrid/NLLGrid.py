58b98fbca4117fad67812b0755960d1b921dd80b
Claudio Satriano <satriano@gmail.com>
2017-06-08 15:48:56 +0200

    BUGFIX: unicode format for Python < 2.7.11

--- nllgrid/NLLGrid.py
+++ nllgrid/NLLGrid.py
@@ -257,7 +257,7 @@ class NLLGrid():
         filename = self.basename + '.buf'
 
         with open(filename, 'rb') as fp:
-            buf = array('f')
+            buf = array(str('f'))
             buf.fromfile(fp, self.nx * self.ny * self.nz)
         if self.type in ['ANGLE', 'ANGLE2D']:
             self.take_off_angles = (TakeOffAngles * len(buf))()
