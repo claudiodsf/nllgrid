8dc7e4fa8cc0d2833c61fbd53b87029f7edab0c6
Claudio Satriano <satriano@gmail.com>
2021-10-12 09:58:45 +0200

    Fix computing epicentral distance

--- nllgrid/NLLGrid.py
+++ nllgrid/NLLGrid.py
@@ -550,7 +550,9 @@ class NLLGrid(object):
         # Special case of 2D grids: y is epicentral distance
         # note: this doesn't work for GLOBAL grids
         if self.nx <= 2:
-            y = np.sqrt(x**2 + y**2)
+            # note: sta_x and sta_y are 0 if not defined in grid header
+            # (e.g., for model grids)
+            y = np.sqrt((x-self.sta_x)**2 + (y-self.sta_y)**2)
             x = self.x_orig
         min_x, max_x, min_y, max_y, min_z, max_z = self.get_extent()
         if not (min_x <= x <= max_x and min_y <= y <= max_y and
