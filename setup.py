9a4e6d587b73a6f2b87e60812ddeeedabd4fad6c
Claudio Satriano <satriano@gmail.com>
2024-09-27 10:52:08 +0200

    Modernize the build system using `pyproject.toml`
    Also, remove vendored `versioneer.py`

--- setup.py
+++ setup.py
@@ -1,46 +1,27 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: CECILL-2.1
-"""setup.py: setuptools control."""
+"""
+A minimal setup script for SourceSpec.
+
+This script ensures compatibility with versioneer
+and dynamically generates a README for correctly resolving paths on GitHub.
+
+All the remaining configuration is in pyproject.toml.
+"""
 from setuptools import setup
 import versioneer
 
+# Dynamically generate the README text for PyPI, replacing local paths
+# with the corresponding URLs on GitHub.
 with open('README.md', 'rb') as f:
-    long_descr = f.read().decode('utf-8').replace(
-        '(CHANGELOG.md)',
-        '(https://cdn.jsdelivr.net/gh/claudiodsf/nllgrid/CHANGELOG.md)'
+    long_description = f.read().decode('utf-8').replace(
+        ': CHANGELOG.md',
+        ': https://github.com/claudiodsf/nllgrid/blob/main/CHANGELOG.md'
     )
 
 setup(
-    name='nllgrid',
-    packages=['nllgrid', ],
-    include_package_data=True,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description='Python class for reading and writing NonLinLoc grid files',
-    long_description=long_descr,
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Claudio Satriano',
-    author_email='satriano@ipgp.fr',
-    url='https://github.com/claudiodsf/nllgrid',
-    license='CeCILL Free Software License Agreement, Version 2.1',
-    platforms='OS Independent',
-    classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre '
-                'License, version 2.1 (CeCILL-2.1)',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: 3.12',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Physics'],
-    python_requires='>=3.7',
-    install_requires=['numpy>=1.0', 'scipy>=0.16', 'pyproj']
-    )
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
+)
